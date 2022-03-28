1f7553ff7f4b9be76197a4f0f70e3beeeb14299a
TenthWolf <64280904+TenthWolf@users.noreply.github.com>
2022-03-28 13:00:21 -0400

    Add files via upload

--- answer.dart
+++ answer.dart
@@ -3,22 +3,24 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   //means that the function stored has to be a property
   // or a pointer at a function
-  final void Function() selectHandler;
+  //final void Function() selectHandler;
 
-  final String answerText;
+  final VoidCallback selectHandler;
 
-  Answer(this.selectHandler, this.answerText);
+  Answer(this.selectHandler);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      color: Colors.grey,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           primary: Colors.blue, //background color
           onPrimary: Colors.white70, //text color
         ),
-        child: Text(answerText),
+        child: Text("Answer 1"),
+        //onPressed: null,
         onPressed: selectHandler,
       ),
     );
